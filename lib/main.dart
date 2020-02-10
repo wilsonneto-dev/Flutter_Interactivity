fee4a14fff3d7e3451904148044a786898c79e30
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-02-10 14:00:17 -0300

    state added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import './screens/home.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "Interactivity",
   home: Home()
 ));
+
+
 
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _Home();
+
+}
+
+class _Home extends State<Home> {
+  String name = '';
+
   @override
   Widget build(BuildContext context) {
-    return Text('Running...');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (String string) {
+                setState((){
+                  name = string;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Please insert your name',
+                prefixIcon: Icon(Icons.add_box)
+              )
+            ),
+            Text("Hello $name!!")
+          ],
+        ),
+        padding: EdgeInsets.all(15),
+      )
+    );
   }
 }
